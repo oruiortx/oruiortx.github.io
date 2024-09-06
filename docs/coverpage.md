f383f9986686d3eef8d062fe1fd6f18d393bae4c
oruiortx <oruiortx@gmail.com>
2024-09-06 11:24:24 +0800

    Updated website files and scripts

---
+++ docs/coverpage.md
@@ -0,0 +1,7 @@
+
+
+<!-- Background image -->
+<!-- ![cover](../wallhaven-5g9rl7_2560x1440.png)
+
+<!-- Background color -->
+<!-- #f0f0f0 -->
--- scripts.js
+++
@@ -1,5 +0,0 @@
-// 侧边栏开关功能
-document.querySelector('.sidebar-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('close');
-  });
-  
